2952df4a981de7de561049eecd5fddbfaaaa8d80
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2021-01-13 12:39:41 -0500

    Time: 168 ms (53.90%), Space: 46.5 MB (43.26%) - LeetHub

--- boats-to-save-people/boats-to-save-people.js
+++ boats-to-save-people/boats-to-save-people.js
@@ -5,23 +5,28 @@
  */
 var numRescueBoats = function(people, limit) {
     
-    people.sort((a,b)=> a-b)
+    
+    people.sort((a,b)=> a-b);
     
     let left = 0;
-    let right = people.length -1;
-    let res = 0
+    let right = people.length-1;
     
-    while(left <= right){
-        res++
-        if(people[left]+ people[right] <= limit){
-            left++
+    let res = 0;
+    
+    while(left <=right){
+        
+        if(people[left] + people[right] <= limit){
+            left++;
             right--
         }else{
-            
             right--
         }
+        res++
+        
         
     }
     
-    return res
+    
+    
+    return res;
 };
