514290f1515b9a47ca23d2131068e7cc5b721fe4
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2021-01-23 18:48:46 -0500

    Time: 136 ms (15.77%), Space: 46.3 MB (9.96%) - LeetHub

--- sort-the-matrix-diagonally/sort-the-matrix-diagonally.js
+++ sort-the-matrix-diagonally/sort-the-matrix-diagonally.js
@@ -7,38 +7,38 @@ var diagonalSort = function(mat) {
     
     let map = {};
     
+    let r = mat.length; let c = mat[0].length;
     
-    for(let i =0; i < mat.length; i++){
-        for(let j=0; j < mat[0].length; j++){
+    
+    for(let i=0; i < r; i++){
+        for(let j=0; j < c; j++){
             
-            let curr = mat[i][j]
-            let key = j - i;
+            let val = mat[i][j];
+            let key = i-j;
             
-            if(map[key] == undefined)map[key] = [];
-            map[key].push(curr)
+            if(map[key] == undefined)map[key] =[];
+            map[key].push(val)
         }
     }
     
-   
+    //sort
+    for(let [key,val] of Object.entries(map))val.sort((a,b)=> a-b);
     
-    for(let [key, val] of Object.entries(map)){
-        val.sort((a,b)=> a-b)
-​
-    }
-     console.log(map)
+    console.log(map)
     
-        for(let i =0; i < mat.length; i++){
-        for(let j=0; j < mat[0].length; j++){
+    
+    for(let i=0; i < r; i++){
+        for(let j =0; j < c; j++){
             
-            let curr = mat[i][j]
-            let key = j - i;
+            let key = i-j;
             
             if(map[key] != undefined){
                 mat[i][j] = map[key].shift()
             }
             
+            
         }
     }
-    return mat
     
+    return mat
 };
