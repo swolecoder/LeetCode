e25f7418afc7bd643ea1ac86b31baee7c3392b87
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2021-02-28 12:58:30 -0500

    Time: 100 ms (37.50%), Space: 45.7 MB (5.55%) - LeetHub

--- delete-and-earn/delete-and-earn.js
+++ delete-and-earn/delete-and-earn.js
@@ -3,20 +3,19 @@
  * @return {number}
  */
 var deleteAndEarn = function(nums) {
+    let arr = new Array(10001).fill(0);
     
-    let counter = new Array(10001).fill(0)
-    
-    for(let i =0; i < nums.length; i++){
-        counter[nums[i]] +=nums[i]
+    for(let i =0; i < nums.length ; i++){
+        arr[nums[i]] += nums[i]
     }
     
-     console.log(counter)
-    let dp= new Array(10001).fill(0);
     
-    for(let i= 1; i < 10001; i++){
-        dp[i] = Math.max(dp[i-1], (dp[i-2]||0) + counter[i])
+    let dp = new Array(10001).fill(0);
+    
+    for(let i = 1; i < 10001; i++){
+        dp[i] = Math.max(dp[i-1], (dp[i-2] || 0) + arr[i])
     }
     console.log(dp)
     
-    return Math.max(...dp)
+    return Math.max(...dp) == -Infinity ? 0 : Math.max(...dp)
 };
