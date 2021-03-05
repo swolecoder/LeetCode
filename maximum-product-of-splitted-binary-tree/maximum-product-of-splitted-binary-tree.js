954f804d5610d5259f9d1a79b08c8828986c7075
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2021-03-05 13:28:16 -0500

    Time: 204 ms (23.53%), Space: 65 MB (45.10%) - LeetHub

---
+++ maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+var mod = Math.pow(10,9) +7
+var maxProduct = function(root) {
+    
+    
+    const totalSum = (root)=>{
+        if(!root)return 0;
+        
+        let left = totalSum(root.left);
+        let right = totalSum(root.right);
+        
+        return (root.val + left + right) % mod
+    }
+    
+    let total  = totalSum(root);
+    let max = -Infinity
+
+    const maxP = (root)=>{
+        if(!root) return 0;
+        
+        let left = maxP(root.left)
+        let right = maxP(root.right)
+        
+        max = Math.max(max, left* (total- left))
+        max = Math.max(max, right* (total- right))
+        
+        return (root.val + left + right)% mod
+        
+    }
+    
+    console.log(total)
+        maxP(root)
+    return max % mod
+};
