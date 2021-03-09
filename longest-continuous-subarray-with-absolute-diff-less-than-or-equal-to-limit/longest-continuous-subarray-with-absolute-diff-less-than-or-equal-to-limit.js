378a9fbc4d3c4f92a4772bc941de0ef7c912f861
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2021-03-09 10:06:56 -0500

    Time: 600 ms (39.13%), Space: 56.8 MB (9.78%) - LeetHub

--- longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.js
+++ longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.js
@@ -4,31 +4,31 @@
  * @return {number}
  */
 var longestSubarray = function(nums, limit) {
-    let min = [];
-    let max = [];
     
+    
+    let min = [];//min array 
+    let max = [];//max
     
     let start = 0;
-    let ans = 0;
     
-    for(let i =0; i< nums.length; i++){
+    let ans = 0
+    
+    for(let i =0; i < nums.length; i++){
+        //clean up
+        while(min.length> 0 && nums[i] <= nums[min[min.length-1]])min.pop();
+        while(max.length >0 && nums[i] >= nums[max[max.length-1]])max.pop();
         
-        while(min.length && nums[i] <= nums[min[min.length-1]])min.pop();
-        
-        while(max.length && nums[i] >= nums[max[max.length-1]])max.pop();
-        
-        
-        min.push(i);
-        max.push(i);
+        min.push(i)
+        max.push(i)
         
         if(nums[max[0]] - nums[min[0]] > limit){
-            
             start++;
             
-            if(start > min[0])min.shift()
-            if(start > max[0])max.shift()
+            if(start > min[0])min.shift();
+            if(start > max[0])max.shift();
         }else{
-            ans = Math.max(ans, i -start+1)
+            console.log(i,start)
+            ans = Math.max(ans, i - start +1)
         }
     }
     
