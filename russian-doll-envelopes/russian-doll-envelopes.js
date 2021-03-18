09d355f0ee63f93e3e5b17244316f028be4106a3
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2021-03-18 10:17:18 -0400

    Time: 512 ms (31.21%), Space: 46.1 MB (7.80%) - LeetHub

--- russian-doll-envelopes/russian-doll-envelopes.js
+++ russian-doll-envelopes/russian-doll-envelopes.js
@@ -2,28 +2,30 @@
  * @param {number[][]} envelopes
  * @return {number}
  */
-var maxEnvelopes = function(envelopes) {
+var maxEnvelopes = function(envelop) {
+     envelop.sort((a,b)=>{
+     if(a[0] === b[0]){
+        return b[1]-a[1]
+      }
+    return a[0] - b[0]
+})
+//create DP
+let N = envelop.length;
+let dp = new Array(N).fill(1);
+let max = 1;
     
-    let N = envelopes.length;
-    
-    let dp = new Array(N).fill(1)
-    
-    envelopes.sort((a,b)=>{
-        if(a[0] == b[0]){
-          return   b[1] - a[1]
-        }
-        return a[0] - b[0]
-    })
-    
-    for(let i =0 ; i < N; i++){
-        for(let j = 0; j < i; j++){
-            if(envelopes[j][1] < envelopes[i][1]){
-                dp[i] = Math.max(dp[i] , dp[j]+1)
-            }
-        }
-    }
-    
-    console.log(dp)
-    
-    return Math.max(...dp) == -Infinity ? 0 : Math.max(...dp)
+for(let i =0; i < N; i++){
+  for(let j =0; j <=i; j++){
+      if(envelop[j][1] < envelop[i][1]){
+           dp[i] = Math.max(dp[i], dp[j] +1)
+            max = Math.max(max, dp[i])
+      }
+  }
+
+
+}
+
+console.log(envelop,dp)
+return max
+
 };
