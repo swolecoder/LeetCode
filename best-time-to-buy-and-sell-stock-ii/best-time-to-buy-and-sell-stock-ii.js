9e6355173403158347b17aaa752b955706fce5aa
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2021-03-20 11:49:58 -0400

    Time: 88 ms (37.92%), Space: 38.8 MB (96.25%) - LeetHub

---
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let diff = 0;
+    
+    for(let i =1; i < prices.length; i++){
+        
+        if(prices[i] > prices[i-1]){
+            diff += prices[i] - prices[i-1]
+        }
+    }
+    
+    return diff
+};
