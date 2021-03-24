879f01915677d191a08f0e323f92155a6fd56adf
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2021-03-24 12:13:33 -0400

    Time: 100 ms (98.53%), Space: 45.4 MB (67.65%) - LeetHub

---
+++ serialize-and-deserialize-n-ary-tree/serialize-and-deserialize-n-ary-tree.js
@@ -0,0 +1,81 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+class Codec {
+  	constructor() {
+        
+    }
+    
+    /** 
+     * @param {Node} root
+     * @return {string}
+     */
+    // Encodes a tree to a single string.
+    serialize = function(root) {
+        
+        
+        let ans = [];
+        
+        function dfs(node){
+            if(!node){
+                // ans.push(node.val)
+                return
+            }
+            
+            ans.push(node.val)
+            if(node.children){
+                ans.push(node.children.length)
+                for(let i =0; i < node.children.length; i++){
+                    dfs(node.children[i])
+                }
+            }else{
+                ans.push("#")
+            }
+        }
+        dfs(root)
+        
+        console.log(ans)
+        
+        return ans.join(",")
+    };
+	
+    /** 
+     * @param {string} data 
+     * @return {Node}
+     */
+    // Decodes your encoded data to tree.
+    deserialize = function(data) {
+        const d = data.split(",")
+        let index = 0;
+        
+        function dfs(node){
+            if(!node.length)return;
+            
+            
+            let val = node[index];
+            index++;
+            if(val == "#")return null;
+            
+            let tree = new Node(val, []);
+            let size = node[index];
+            index++
+           for(let i=0; i < size; i++){
+                tree.children.push((dfs(node)))
+            }
+            
+         
+            return tree
+        }
+        
+       return dfs(d)
+    };
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.deserialize(codec.serialize(root));
