20de576ca3a687aaa0829de43368c0a6b037cc41
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2021-03-28 13:30:17 -0400

    Time: 204 ms (25.66%), Space: 47.9 MB (7.08%) - LeetHub

--- maximum-length-of-pair-chain/maximum-length-of-pair-chain.js
+++ maximum-length-of-pair-chain/maximum-length-of-pair-chain.js
@@ -3,29 +3,20 @@
  * @return {number}
  */
 var findLongestChain = function(pairs) {
-    /*
-       [[1,2], [2,3], [3,4]]
-         a, b.   c,d
-       [1,2,1]
     
-    
-    
-    */
-    
-    pairs.sort((a,b)=> a[0] - b[0])
-    
-    let N = pairs.length
-    let dp = new Array(N).fill(1)
-    
-    for(let i =0; i < N; i++){
-        for(let j = 0; j < i ; j++){
-            if(pairs[j][1] < pairs[i][0]){
-                dp[i] = Math.max(dp[i], dp[j]+1)
-            }
-        }
-    }
-    
-    console.log(dp)
-    
-    return Math.max(...dp)
+      pairs.sort((a,b)=> a[0] - b[0]);
+      let n = pairs.length;
+    console.log(pairs)
+      let dp = new Array(n).fill(1);
+      let max = 1;
+      for(let i =1;i < n; i++){
+          for(let j = 0; j <= i; j++){
+              if(pairs[j][1] < pairs[i][0]){
+                  dp[i] = Math.max(dp[i], dp[j]+1);
+                  max = Math.max(dp[i], max)
+              }
+          }
+      }
+     console.log(dp)
+    return max
 };
