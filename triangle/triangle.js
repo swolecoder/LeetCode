6c0acb195577e0837ebaefb21eb2642a557d68bb
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2021-04-21 20:28:54 -0400

    Time: 92 ms (15.67%), Space: 39.5 MB (39.60%) - LeetHub

--- triangle/triangle.js
+++ triangle/triangle.js
@@ -3,13 +3,15 @@
  * @return {number}
  */
 var minimumTotal = function(t) {
-    let dp = t[t.length-1]
-
-    for(let i=t.length-2; i>=0; i--){
-        for(let j =0; j < t[i].length; j++){
-            dp[j] = t[i][j] + Math.min(dp[j], dp[j+1])
-        }
-    }
+    let dp = t[t.length-1];
     
-    return dp[0]
+   for(let i = t.length -2; i >= 0; i--){
+           
+      for(let j =0; j < t[i].length; j++){
+              dp[j] = Math.min(dp[j], dp[j+1]) + t[i][j]
+      }     
+   }
+        
+        console.log(dp)
+        return dp[0]
 };
