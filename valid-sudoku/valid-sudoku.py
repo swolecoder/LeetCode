09236655ec2da22fff342c5db6036a0de5b33e01
Ashish Ranjan <41638972+swolecoder@users.noreply.github.com>
2021-09-01 10:16:35 -0400

    Time: 198 ms (5.01%), Space: 14.3 MB (42.84%) - LeetHub

---
+++ valid-sudoku/valid-sudoku.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row = collections.defaultdict(set)
+        col = collections.defaultdict(set)
+        subSquare = collections.defaultdict(set) # key = (r//3,c//3)
+        
+        for r in range(9):
+            for c in range(9):
+                
+                if(board[r][c] == "."):
+                    continue
+                
+                
+                if board[r][c] in row[r] or board[r][c] in col[c] or board[r][c] in subSquare[(r//3,c//3)]:
+                    return False
+                
+                row[r].add(board[r][c])
+                col[c].add(board[r][c])
+                subSquare[(r//3,c//3)].add(board[r][c])
+        
+        return True
+        
